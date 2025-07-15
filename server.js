091187df6aa4f76c3adf3e46c571411c6851d3fd
blelian <bullsurus@gmail.com>
2025-07-15 11:00:34 +0200

    change

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 // Contacts API routes
 app.use('/contacts', contactRoutes);
 
+
 // Default route
 app.get('/', (req, res) => {
   res.send('Welcome to the Contacts API');
